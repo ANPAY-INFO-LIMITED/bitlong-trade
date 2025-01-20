1dc61fa978f77a28e7e4f2c4431903955d552f68
youngzhenhao <2499342961@qq.com>
2025-01-20 11:12:11 +0800

    update: /assetMoreInfo router

--- routers/assetMoreInfo.go
+++ routers/assetMoreInfo.go
@@ -7,17 +7,28 @@ import (
 )
 
 func SetupAssetMoreInfo(router *gin.Engine) *gin.Engine {
+	assetMoreInfo := router.Group("/asset_more_info")
+	{
+		assetMoreInfo.GET("/get/asset_balance_info/count", handlers.GetAssetBalanceInfoCount)
+		assetMoreInfo.GET("/get/asset_balance_info", handlers.GetAssetBalanceInfo)
+		assetMoreInfo.GET("/get/account_asset_transfer/count", handlers.GetAccountAssetTransferCount)
+		assetMoreInfo.GET("/get/account_asset_transfer", handlers.GetAccountAssetTransfer)
+		assetMoreInfo.GET("/get/asset_managed_utxo_info/count", handlers.GetAssetManagedUtxoInfoCount)
+		assetMoreInfo.GET("/get/asset_managed_utxo_info", handlers.GetAssetManagedUtxoInfo)
+		assetMoreInfo.GET("/get/asset_transfer_50", handlers.GetAssetTransferCombinedSliceByAssetIdLimit)
+	}
+
 	username := config.GetLoadConfig().AdminUser.Username
 	password := config.GetLoadConfig().AdminUser.Password
 
-	authorized := router.Group("/asset_more_info", gin.BasicAuth(gin.Accounts{
+	authorized := router.Group("/asset_more_info/auth", gin.BasicAuth(gin.Accounts{
 		username: password,
 	}))
-	authorized.GET("/get/asset_balance_info_count", handlers.GetAssetBalanceInfoCount)
+	authorized.GET("/get/asset_balance_info/count", handlers.GetAssetBalanceInfoCount)
 	authorized.GET("/get/asset_balance_info", handlers.GetAssetBalanceInfo)
-	authorized.GET("/get/account_asset_transfer_count", handlers.GetAccountAssetTransferCount)
+	authorized.GET("/get/account_asset_transfer/count", handlers.GetAccountAssetTransferCount)
 	authorized.GET("/get/account_asset_transfer", handlers.GetAccountAssetTransfer)
-	authorized.GET("/get/asset_managed_utxo_info_count", handlers.GetAssetManagedUtxoInfoCount)
+	authorized.GET("/get/asset_managed_utxo_info/count", handlers.GetAssetManagedUtxoInfoCount)
 	authorized.GET("/get/asset_managed_utxo_info", handlers.GetAssetManagedUtxoInfo)
 	authorized.GET("/get/asset_transfer_50", handlers.GetAssetTransferCombinedSliceByAssetIdLimit)
 	return router
